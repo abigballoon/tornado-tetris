39264d464303f1aa414c3e116183c5ab34fc4639
sillk4977 <497783539@qq.com>
2015-12-30 19:54:04 +0800

    Create urls.py
    run this file to start the program

---
+++ urls.py
@@ -0,0 +1,45 @@
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os.path
+
+import views
+
+from tornado.options import define, options
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", views.MainHandler),
+            (r"/signup", views.SignupHandler),
+            (r"/login", views.LoginHandler),
+            (r"/tetris",views.TetrisHandler),
+            (r"/tetris/control", views.TetrisControlHandler),
+            (r"/tetrissocket", views.TetrisSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.PeriodicCallback(views.GeneralData.cleanSTT,views.GeneralData.cleanTime).start()
+    io_loop = tornado.ioloop.IOLoop.current()
+    io_loop.start()
+
+
+if __name__ == "__main__":
+    main()
